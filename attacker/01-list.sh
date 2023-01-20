3a48be7cb7af788c6f84a945c57f0f61abf9320c
root <root@SJ-LAP-142.VIRSEC.local>
2023-01-20 11:22:55 -0700

    changed attacker rce payload to: touch /var/tmp/owned changed attacker script default port from 4040 to 8080

--- attacker/01-list.sh
+++ attacker/01-list.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -x
-curl -i http://localhost:4040/accounts; echo
+curl -i http://10.48.18.26:8080/accounts; echo
--- attacker/02-add.sh
+++ attacker/02-add.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -x
-curl -H 'Content-Type:application/json' -i -X POST -d '{"customerName":"Jarkko","accountType":"Checking", "accountNumber" : "12345679", "balance" : 10000, "accountNotes":["java.util.HashMap",{"randomNotes":"BlahBlah"}]}' http://localhost:4040/accounts; echo
+curl -H 'Content-Type:application/json' -i -X POST -d '{"customerName":"Jarkko","accountType":"Checking", "accountNumber" : "12345679", "balance" : 10000, "accountNotes":["java.util.HashMap",{"randomNotes":"BlahBlah"}]}' http://10.48.18.26:8080/accounts; echo
